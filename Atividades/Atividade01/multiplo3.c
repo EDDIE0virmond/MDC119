2bb628fb187d5633e4180eb6b830dd0ae001a1d3
eduvirmond <59213021+EDDIE0virmond@users.noreply.github.com>
2020-03-04 08:42:08 -0300

    mulotiplos de 3

---
+++ Atividades/Atividade01/multiplo3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int contador = 0 ;
+    int i , k ;
+    for (i==0 ; i ++ ; i<100):{
+        contador ++ ;
+        k = contador / 3 ;
+        if (k = int){
+            printf ("%i",k);
+        }
+    }
+
+    /* code */
+    return 0;
+}
